7922aa63c4e06ce13c76fe7a513711d955d1647d
tecniv <vguell57@gmail.com>
2025-07-29 10:43:32 +0200

    demande nom, hello+nom

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,14 @@
 package org.example;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println("What is your name ?");
+        Scanner scanner = new Scanner(System.in);
+        String name = scanner.nextLine();
+        System.out.println("Hello " + name);
+
+
     }
 }
